f2bff8e753a5b861adb7aa71507a2223d302f2c4
Vipin mavi <vipinmavi@vinay-MacBook-Pro.local>
2025-11-28 08:22:48 +0530

    First algorithm done of 'Find the first non-repeating character from a String'

---
+++ string/findFirstNonRepeatedChar.js
@@ -0,0 +1,20 @@
+const UserInputString = "Iamvipinmavi";
+
+function findTheChar(stringToOperate) {
+    let result = null;
+    outerLoop :for (let i = 0; i < stringToOperate.length-1; i++) {
+        const elementLookingFor = stringToOperate[i];
+        for (let j = i+1; j < stringToOperate.length; j++) {
+            const machingFrom = stringToOperate[j];
+            if(elementLookingFor.toUpperCase() == machingFrom.toUpperCase()){
+                break;
+            }else if( j ==  stringToOperate.length-1 ){
+                result = elementLookingFor;
+                break outerLoop;
+            }
+        }
+    }
+    return result;
+}
+let foundNonRepeatedChar = findTheChar(UserInputString);
+console.log(foundNonRepeatedChar);
